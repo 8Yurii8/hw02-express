a0499c10b8cbe803bf374598aae31c5d7d440e17
8Yurii8 <114691896+8Yurii8@users.noreply.github.com>
2023-07-25 22:20:15 +0300

    start hw03

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,18 +1,12 @@
 import express from "express";
-import {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} from "./controllers/controllers.js";
+import contactControllers from "./controllers/controllers.js";
 
 const contactsRouter = express.Router();
 
-contactsRouter.get("/", listContacts);
-contactsRouter.get("/:id", getContactById);
-contactsRouter.post("/", addContact);
-contactsRouter.delete("/:id", removeContact);
-contactsRouter.put("/:id", updateContact);
+contactsRouter.get("/", contactControllers.listContacts);
+contactsRouter.get("/:id", contactControllers.getContactById);
+contactsRouter.post("/", contactControllers.addContact);
+contactsRouter.delete("/:id", contactControllers.removeContact);
+contactsRouter.put("/:id", contactControllers.updateContact);
 
 export default contactsRouter;
